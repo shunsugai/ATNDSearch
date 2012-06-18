b1cb3b9d6c14bbad49a0dd4a8b41b5c8902e7a83
Shun Sugai <sugaishun@gmail.com>
2012-06-18 18:15:41 +0900

    some refoctoring

--- src/com/sugaishun/atndsearch/EventDetailActivity.java
+++ src/com/sugaishun/atndsearch/EventDetailActivity.java
@@ -25,8 +25,10 @@ public class EventDetailActivity extends Activity {
 		setContentView(R.layout.eventdetail);
 		getDataThroughIntent();	
 		setBrowser();
-		
-		// Ad
+		setAd();
+	}
+	
+	private void setAd() {
 		// Create the adView
 		adView = new AdView(this, AdSize.BANNER, MY_AD_UNIT_ID);
 
@@ -38,7 +40,7 @@ public class EventDetailActivity extends Activity {
 		layout.addView(adView);
 
 		// Initiate a generic request to load it with an ad
-		adView.loadAd(new AdRequest());
+		adView.loadAd(new AdRequest());		
 	}
 	
 	private void getDataThroughIntent() {
