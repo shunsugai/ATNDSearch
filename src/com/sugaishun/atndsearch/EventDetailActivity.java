9e8e3ef16dbfe583fb4ed43d8b0a8f548cdf15e7
Shun Sugai <sugaishun@gmail.com>
2012-06-24 11:32:13 +0900

    Refactoring

--- src/com/sugaishun/atndsearch/EventDetailActivity.java
+++ src/com/sugaishun/atndsearch/EventDetailActivity.java
@@ -4,7 +4,6 @@ import com.google.ads.AdRequest;
 import com.google.ads.AdSize;
 import com.google.ads.AdView;
 
-import net.java.textilej.parser.markup.textile.TextileDialect;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -61,8 +60,10 @@ public class EventDetailActivity extends Activity {
 	}
 	
 	private void setBrowser() {
+		EventDetailHTMLBuilder htmlBuilder = new EventDetailHTMLBuilder(title, date, address, description);
+		String html = htmlBuilder.getStringHtml();
 		browser = (WebView) findViewById(R.id.Webkit);
-		browser.loadDataWithBaseURL("empty", buildHTML(), "text/html", "UTF-8", null);
+		browser.loadDataWithBaseURL("empty", html, "text/html", "UTF-8", null);
 	}
 	
 //	class MyWebView extends WebView {
@@ -115,26 +116,4 @@ public class EventDetailActivity extends Activity {
 //			}
 //		};
 //	}
-	
-	private String TextileToHtml(String text) {
-		String html = "";
-		try {
-			html = EventDetailHelper.textToHtml(text, TextileDialect.class, false);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return html;
-	}
-	
-	private String buildHTML() {
-		String css = "body,h1,h2,p{padding:0;margin:0}body{background-color:#F5F5F5;font-size:13px;padding:10px}h1{font-size:20px}h2{font-size:16px}";
-		String html = "<html><head><meta http-equiv=\"content-type\" content=\"text/html;charset=UTF-8\">"
-				+ "<style type=\"text/css\">" + css + "</style></head><body>"
-				+ "<h1>" + title + "</h1>"
-				+ "<h2>日時:</h2>" + "<p>" + DateHelper.shortDate(date) + " " + DateHelper.time(date) + "</p>"
-				+ "<h2>場所:</h2>" + "<p>" + address + "</p>"
-				+ "<h2>イベント概要:</h2>" + TextileToHtml(description) 
-				+ "</body></html>";
-		return html;
-	}
 }
