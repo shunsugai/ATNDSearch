8330b0027d0f36c5be8af559e69ca9708c72c618
Shun Sugai <sugaishun@gmail.com>
2012-07-24 21:16:44 +0900

    listをlayoutから作るようにした

--- src/com/sugaishun/atndsearch/EventAdapter.java
+++ src/com/sugaishun/atndsearch/EventAdapter.java
@@ -7,6 +7,7 @@ import org.joda.time.DateTime;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -16,11 +17,11 @@ import android.text.TextUtils;
 
 public class EventAdapter extends BaseAdapter {
 //	private static final String TAG = EventAdapter.class.getSimpleName();
-	private Context ctx;
+	private Context context;
 	private List<Event> events;
 	
 	public EventAdapter(Context context, List<Event> events) {
-		this.ctx = context;
+		this.context = context;
 		this.events = events;
 	}
 	
@@ -40,54 +41,29 @@ public class EventAdapter extends BaseAdapter {
 	}
 
 	@Override
-	public View getView(int position, View contentView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		Event event = events.get(position);
-		return new EventListView(ctx, event.getTitle(), event.getDate(), event.getCatchcopy());
-	}
-	
-	private final class EventListView extends LinearLayout {
-		private TextView v_title;
-		private TextView v_date;
-		private TextView v_catchcopy;
-
-		public EventListView(Context context, String title, String date, String catchcopy) {
-			super(context);
-			
-			setOrientation(LinearLayout.VERTICAL);
-			setBackgroundDrawable(context.getResources().getDrawable(R.drawable.list_background));
-			setPadding(15, 5, 15, 5);
-			
-			LinearLayout.LayoutParams param = 
-					new LinearLayout.LayoutParams(
-							ViewGroup.LayoutParams.WRAP_CONTENT,
-							ViewGroup.LayoutParams.WRAP_CONTENT);
-			param.setMargins(3, 3, 3, 3);
-			
-			v_date = new TextView(context);
-			v_date.setText(getFormattedDateTime(date));
-			v_date.setTextSize(13f);
-			v_date.setTextColor(Color.DKGRAY);
-			addView(v_date, param);
-
-			v_title = new TextView(context);
-			v_title.setText(title);
-			v_title.setTextSize(16f);
-			v_title.setTextColor(Color.BLACK);
-			v_title.setMaxLines(2);
-			v_title.setMinLines(2);
-			v_title.setEllipsize(TextUtils.TruncateAt.END);
-			addView(v_title, param);
-			
-			v_catchcopy = new TextView(context);
-			v_catchcopy.setText(catchcopy);
-			v_catchcopy.setTextSize(13f);
-			v_catchcopy.setTextColor(Color.DKGRAY);
-			v_catchcopy.setSingleLine(true);
-			v_catchcopy.setEllipsize(TextUtils.TruncateAt.END);
-			addView(v_catchcopy, param);
+		View row = convertView;
+		
+		if (row == null) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			row = inflater.inflate(R.layout.row, parent, false);
 		}
+		
+		ViewHolder holder = (ViewHolder) row.getTag();
+		
+		if (holder == null) {
+			holder = new ViewHolder(row);
+			row.setTag(holder);
+		}
+		
+		holder.date.setText(getFormattedDateTime(event.getDate()));
+		holder.title.setText(event.getTitle());
+		holder.subTitle.setText(event.getCatchcopy());
+		
+		return row;
 	}
-	
+
 	public String getFormattedDateTime(String ISO8601DateTime) {
 		DateTime dt = new DateTime(ISO8601DateTime);
 		return dt.toString("yyyy年MM月dd日 HH:mm", Locale.JAPAN);
